c215e707714e4d011ac5d70304dad59af95eddcf
Eduard Bopp <eduard.bopp@aepsil0n.de>
2016-05-21 18:50:42 +0200

    Use more readable variable name

--- src/main.rs
+++ src/main.rs
@@ -30,20 +30,20 @@ fn settings() -> WindowSettings {
 }
 
 fn main() {
-    let c = CanvasSize::new_from_center(900, 600, [-0.5, 0.0], 1.0);
+    let canvas = CanvasSize::new_from_center(900, 600, [-0.5, 0.0], 1.0);
     let max = 1000u32;
 
     let mut driver2d = Driver2d::new(settings());
 
     let output = start(
         Component {
-            init: app::init(c, max),
+            init: app::init(canvas, max),
             update: app::update,
             view: app::view,
             effect: |_, _| None
         },
         interpret(driver2d.output(), app::intent)
-        );
+    );
 
     driver2d.run(output);
 }
