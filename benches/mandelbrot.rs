80a06921093ddecc766bb3b9cae435dd5426abd9
André-Patrick Bubel <code@andre-bubel.de>
2016-06-21 12:14:15 +0200

    Bench make_image

--- benches/mandelbrot.rs
+++ benches/mandelbrot.rs
@@ -13,6 +13,7 @@ use rust_mpfr::mpfr::*;
 #[bench]
 fn bench_iterate_float(b: &mut Bencher) {
     let max = 1000;
+
     b.iter(|| { iterate::<f64>(-0.5f64, 0f64, max) });
 }
 
@@ -21,6 +22,7 @@ fn bench_iterate_mpfr(b: &mut Bencher) {
     let max = 1000;
     let m1 = Mpfr::new_from_str("-0.5", 10).unwrap();
     let m2 = Mpfr::new_from_str("0.0", 10).unwrap();
+
     b.iter(|| { iterate::<Mpfr>(m1.clone(), m2.clone(), max) });
 }
 
@@ -28,5 +30,15 @@ fn bench_iterate_mpfr(b: &mut Bencher) {
 fn bench_calculate_all(b: &mut Bencher) {
     let max = 1000;
     let c = CanvasSize::new_from_center(50, 50, [mpfr!(-0.5), mpfr!(0.0)], mpfr!(1.0));
+
     b.iter(|| calculate_all(c.clone(), max));
+}
+
+#[bench]
+fn bench_make_image(b: &mut Bencher) {
+    let max = 1000;
+    let c = CanvasSize::new_from_center(50, 50, [mpfr!(-0.5), mpfr!(0.0)], mpfr!(1.0));
+    let data = calculate_all(c.clone(), max);
+
+    b.iter(|| make_image(data.clone(), c.clone(), max));
 }
